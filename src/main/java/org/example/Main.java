272c0f055aafdc84660e4d7f7f409fe7b4967c03
Therenegadee <melkindaniil@yandex.ru>
2023-07-03 15:43:45 +0300

    First v. w/ selenium

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BrowserConnection browserConnection = new BrowserConnection();
+        browserConnection.prepareToParse();
+
+    }
+}
