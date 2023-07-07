1ffe5eaa3a5421af0027f921bb6601531760007a
Therenegadee <melkindaniil@yandex.ru>
2023-07-07 20:15:37 +0300

    2-nd v. Paginator is based only on Silenium. Jsoup is deleted from pom.xml

--- src/main/java/org/example/Paginator.java
+++ src/main/java/org/example/Paginator.java
@@ -16,17 +16,9 @@ public class Paginator {
     Paginator() {
     }
 
-    public Document clickNextPageButton (WebDriver driver) {
+    public void clickNextPageButton (WebDriver driver) {
         WebElement nextPageButton = driver.findElement(By.cssSelector("body > div > div.pro_field > div > div > a.next"));
         nextPageButton.click();
-        Connection connection = Jsoup.connect(driver.getCurrentUrl());
-        Document pageForParse = null;
-        try {
-            pageForParse = connection.get();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return pageForParse;
     }
 
 }
